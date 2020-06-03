51125179e2c069b1f0e230bc0f1b64d3ae5661aa
helang12345 <hrl727273603@163.com>
2020-06-03 23:32:41 +0800

    更新数据库

--- 毕业设计代码与数据库/House.sql
+++ 毕业设计代码与数据库/House.sql
@@ -8,55 +8,56 @@ UserID int identity(1,1) primary key not null,--编号
 UserName nvarchar(20) not null,--姓名
 UserSex int default(0),--性别，0为男，1为女
 UserPhone nvarchar(20) not null,--电话
-UserIdentity nvarchar(20) not null,--身份证
-UserLoginName nvarchar(20) not null,--登录名
-UserPassword nvarchar(20) not null,--密码
+UserLoginName nvarchar(50) not null,--登录名
+UserPassword nvarchar(50) not null,--密码
+[Role] int ,--角色 0普通用户 1超级管理员
 UState int default(0),--状态，0默认存在
 )
 --销售表
 create table Salesman
 (
 SalesmanID int identity(1,1) primary key not null,--编号
+SalesmanName nvarchar(20) not null,--姓名
+SalesmanSex int default(0),--性别，0为男，1为女
+SalesmanPhone nvarchar(20) not null,--电话
+SalesmanLoginName nvarchar(50) not null,--登录名
+SalesmanPassword nvarchar(50) not null,--密码
 SalesmanPhoto nvarchar(20),--销售头像
 UState int default(0),--状态，0默认存在
 )
---销售用户关系表
-create table Relationship
-(
-RelationshipID int identity(1,1) primary key not null,
-UserID int foreign key (UserID) references Userd (UserID),
-SalesmanID int foreign key (SalesmanID) references Salesman (SalesmanID),
-UState int default(0),--状态，0默认存在
-)
+
 --出售信息表
 create table Sell
 (
 SellID int identity(1,1) primary key not null,--编号
-SellAddress nvarchar(20),--所在城市
-SellVillage nvarchar(20),--所在小区
+SellAddress nvarchar(50),--所在城市
+SellVillage nvarchar(50),--所在小区
+SellFloor int,--楼层
 SellArea  nvarchar(20),--面积
 SellAreaTo nvarchar(20),--实际面积
 SellPrice nvarchar(20),--每平价格
 SellPriceTo nvarchar(20),--总价格
-SellFloor int,--楼层
-SellAType nvarchar(20),--建筑类型
-SellBStructure nvarchar(20),--建筑结构
-SellHStructurechar nvarchar(20),--房屋类型
+SellAType nvarchar(20),--建筑类型（板塔结构）
+SellBStructure nvarchar(20),--建筑结构（钢混结构）
+SellHStructurechar nvarchar(20),--房屋类型（3室1厅）
 SellRenovation nvarchar(20),--装修情况
-SellScale nvarchar(20),--梯户情况
-SellPhoto nvarchar(20),--实景图
+SellScale nvarchar(20),--梯户情况（1梯1户）
 SellHeating nvarchar(20),--供暖方式
 SellOrientation nvarchar(20),--房屋朝向
 SellTime Date,--看房时间
-SellTraffic nvarchar(20),--交通出行
-SellInfrastructure nvarchar(20),--周边配套
-SellTaxation nvarchar(20),--税费服务
-SellIntroduce nvarchar(20),--户型介绍
-SellCore nvarchar(50),--核心卖点
-SellLabel nvarchar(20),--标签
 UserID int foreign key (UserID) references Userd (UserID),--房主
 SalesmanID int foreign key (SalesmanID) references Salesman (SalesmanID),--销售
 UState int default(0),--状态，0默认存在
+)
+--出售卖点表
+create table Selling
+(
+SellingTraffic nvarchar(200),--交通出行
+SellingInfrastructure nvarchar(200),--周边配套
+SellingTaxation nvarchar(200),--税费服务
+SellingIntroduce nvarchar(200),--户型介绍
+SellingVillage nvarchar(200),--小区介绍
+SellID int foreign key (SellID) references Sell (SellID),--出售信息外键
 )
 --交易属性表
 create table Transactions
@@ -67,26 +68,40 @@ TransactionCommodity nvarchar(20),--交易权属（是否为商品房）
 TransactionOldTransaction nvarchar(20),--上次交易信息
 TransactionPurpose nvarchar(20),--房屋用途（住房、店铺）
 TransactionYears int,--房屋年限
-TransactionPropertyright nvarchar(20),--产权所有
-TransactionMortgage nvarchar(20),--抵押信息
-TransactionPOC nvarchar(20),--房本备份
-SellID int foreign key (SellID) references Sell (SellID),--销售
+TransactionPropertyright nvarchar(2000),--产权所有
+TransactionMortgage nvarchar(200),--抵押信息
+TransactionPOC nvarchar(200),--房本备份
+SellID int foreign key (SellID) references Sell (SellID),--出售信息外键
+)
+--出售图片表
+create table SImg
+(
+ID int identity(1,1) primary key not null,--编号
+SellID int foreign key (SellID) references Sell (SellID),--出售信息外键
+Photo nvarchar(50),--照片名
+)
+--收藏表
+create table SCollection
+(
+ID int identity(1,1) primary key not null,--编号
+SellID int foreign key (SellID) references Sell (SellID),--出售信息外键
+UserID int foreign key (UserID) references Userd (UserID),--房主
 )
 --出租信息表
 create table Lease
 (
 LeaseID int identity(1,1) primary key not null,--编号
+LeaseAddress nvarchar(50),--所在城市
+LeaseVillage nvarchar(50),--所在小区
+LeaseFloor int,--楼层
 LeaseArea nvarchar(20),--面积
-LeaseAddress nvarchar(20),--地址
 LeaseMaintain nvarchar(20),--维护时间
 LeaseHStructure nvarchar(20),--房屋类型
 LeasePrice nvarchar(20),--租金
 LeaseLease nvarchar(20),--租赁方式
 LeaseCheckin nvarchar(20),--入住情况
 LeaseParkinglot nvarchar(20),--车位
-LeaseFloor int,--楼层
 LeaseOrientation nvarchar(20),--朝向
-LeasePhoto nvarchar(20),--实景图
 LeaseElevator int default(0),--电梯（0表示存在）
 LeaseWaterint nvarchar(20),--用水
 LeaseElectric nvarchar(20),--用电
@@ -94,9 +109,6 @@ LeaseGas nvarchar(20),--燃气
 LeaseHeating nvarchar(20),--供暖
 LeaseLeaseTerm nvarchar(20),--租期
 LeaseTime Date,--看房时间
-LeaseTraffic nvarchar(20),--交通出行
-LeaseInfrastructure nvarchar(50),--周边配套
-LeaseVillage nvarchar(50),--小区介绍
 UserID int foreign key (UserID) references Userd (UserID),--房主
 SalesmanID int foreign key (SalesmanID) references Salesman (SalesmanID),--销售
 UState int default(0),--状态，0默认存在
@@ -114,5 +126,19 @@ FacilitiesBed int default(0),--床，0默认存在
 FacilitiesWIFI int default(0),--WiFi，0默认存在
 FacilitiesNaturalgas int default(0),--天然气，0默认存在
 FacilitiesHeating int default(0),--暖气，0默认存在
-LeaseID int foreign key (LeaseID) references Lease (LeaseID),--房主
+LeaseID int foreign key (LeaseID) references Lease (LeaseID),--出租信息外键
+)
+--出租图片表
+create table LImg
+(
+ID int identity(1,1) primary key not null,--编号
+LeaseID int foreign key (LeaseID) references Lease (LeaseID),--出租信息外键
+Photo nvarchar(50),--照片明
+)
+--收藏表
+create table LCollection
+(
+ID int identity(1,1) primary key not null,--编号
+LeaseID int foreign key (LeaseID) references Lease (LeaseID),--出租信息外键
+UserID int foreign key (UserID) references Userd (UserID),--房主
 )
---
+++ 毕业设计代码与数据库/House/House/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="House.MvcApplication" Language="C#" %>
