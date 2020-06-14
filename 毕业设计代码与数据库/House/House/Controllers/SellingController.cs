820dd16921d464b8b4b3b42607ab3c247e4cf6b9
helang12345 <hrl727273603@163.com>
2020-06-14 20:26:51 +0800

    跟新界面

---
+++ 毕业设计代码与数据库/House/House/Controllers/SellingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace House.Controllers
+{
+    public class SellingController : Controller
+    {
+        // GET: Selling
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
